beb78250659f96a0f30248537ae1729555c23e64
Mike Fienen <mnfienen@usgs.gov>
2021-07-29 16:57:44 -0500

    refactor: hca.py to analysis_project.py

---
+++ hicap_analysis/analysis_project.py
@@ -0,0 +1,54 @@
+from wells import Well, WellResponse
+import numpy as np
+import pandas as pd
+
+class Project():
+    def __init__(self) -> None:
+        """made up of multiple Well objects
+        instantiate Well objects for existing and proposed
+        have all names, properties, locations, etc.
+
+        Args:
+            T ([type]): [description]
+            S ([type]): [description]
+            time (list, optional): [description]. Defaults to [].
+            Q (list, optional): [description]. Defaults to [].
+            stream_locs (dict, optional): [description]. Defaults to {}.
+            stream_apportionment (dict, optional): [description]. Defaults to {}.
+            drawdown_locs (list, optional): [description]. Defaults to [].
+        """
+        pass
+
+    def populate_from_yaml(self, ymlfile):
+        pass
+
+    def populate(self, T = -9999, S = -9999, existing_wells = {}, )
+    def existing_wells(self):
+        """dictionary of existing Well objects for cumulative impacts
+        """
+
+        pass
+
+    def proposed_wells(self):
+        """dictionary of proposed Well object(s)
+        """
+        pass
+
+    def existing_impacts(self):
+        """possibly precalculated so a load or calculation option
+        """
+        
+        pass
+
+    def proposed_impacts(self):
+        """
+        calculate and store all the new impacts
+        """
+        pass
+
+    def calculate_significance(self):
+        """[summary]
+        """
+        pass
+
+
